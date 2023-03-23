f2237ab450f3cf9fbb39d7a0e4c789948437ea7a
Aryanstha <aryanstha4859@gmail.com>
2023-03-23 17:02:52 +0545

    first commit
    Co-Authored-By: Serina Pokharel <114247122+serinapokharel@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.listen(3000, () => {
+
+  console.log("Application started and Listening on port 3000");
+});
+
+// server css as static
+app.use(express.static(__dirname));
+
+// get our app to use body parser 
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/src/index.html");
+});
+
+
+
+app.post("/", (req, res) => {
+  var subName = req.body.yourname
+  var subEmail = req.body.youremail;
+ res.send("Hello " + subName + ", Thank you for subcribing. You email is " + subEmail);
+});
